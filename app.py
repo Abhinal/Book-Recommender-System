50360d1b2c0a1ad9b74dde6dd753effb25e0f970
Abhinav Agarwal <abhinavagarwalsps@gmail.com>
2021-10-28 22:13:30 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -32,9 +32,4 @@ book_list = books["Title"].values
 selected_book = st.selectbox("Select Your Book", book_list)
 
 if st.button('Search'):
-    # input_query = np.array([[km_driven, selected_fuel_type, selected_seller_type, selected_transmission,
-                        #    selected_owner, mileage, engine, max_power, seats, 2020-purchase_year, selected_brand]])
-    # price = pipe.predict(input_query)[0]
     get_name(selected_book)
-    # st.header(f"{selected_book}")
-#     get_name(selected_book,x)
